464b73aeeeff4582f1e066421e290b75afaec836
ShotaOnishi <showta.0804@gmail.com>
2016-11-08 01:32:25 +0900

    test

--- model/observers/message/local_message.rb
+++ model/observers/message/local_message.rb
@@ -4,9 +4,10 @@ class LocalMessage
     if Message.is_postback?(event)
     elsif Message.is_message?(event)
       if /(雑談)/ =~ event.message['text']
+        event.message["text"] = event.message["text"].delete("雑談")
         text = []
-        for 1..3 do |i|
-          text[i] = get_user_local_bot_reply(event.message["text"].delete("雑談"))
+        for i in 1..3 do
+          text[i] = get_user_local_bot_reply(event.message["text"])
         end
         Message.reply(event, output(text))
       end
@@ -17,16 +18,16 @@ class LocalMessage
     [
       {
         type: "text",
-        text: text[1]
+        text: text[1].to_s
       },
       {
         type: "text",
-        text: text[2]
+        text: text[2].to_s
       },
       {
         type: "text",
-        text: text[3]
-      },
+        text: text[3].to_s
+      }
     ]
   end
 
