276b38179e151f420a2fca49c1a372d57386564c
ShotaOnishi <showta.0804@gmail.com>
2016-11-08 02:04:48 +0900

    test

--- model/observers/message/local_message.rb
+++ model/observers/message/local_message.rb
@@ -6,7 +6,7 @@ class LocalMessage
       if /(雑談)/ =~ event.message['text']
         event.message["text"] = event.message["text"].delete("雑談")
         text = []
-        for i in 1..3 do
+        for i in 1..2 do
           text[i] = get_user_local_bot_reply(event.message["text"])
         end
         Message.reply(event, output(text))
@@ -23,10 +23,6 @@ class LocalMessage
       {
         type: "text",
         text: text[2].to_s
-      },
-      {
-        type: "text",
-        text: text[3].to_s
       }
     ]
   end
@@ -35,11 +31,16 @@ class LocalMessage
     require 'rest-client'
     response = RestClient.get 'https://chatbot-api.userlocal.jp/api/chat', { params: { key: ENV['USR_LOCAL_API_KEY'], message: CGI.escape(word)} }
     response_json = JSON.parse(response)
-    cat_res =RestClient.get 'https://chatbot-api.userlocal.jp/api/character', { params: { key: ENV['USR_LOCAL_API_KEY'], message: "眠いな", character_type: "cat"} }
+    cat_res =RestClient.get 'https://chatbot-api.userlocal.jp/api/character', { params: { key: ENV['USR_LOCAL_API_KEY'], message: response_json['result'], character_type: "cat"} }
+    dog_res =RestClient.get 'https://chatbot-api.userlocal.jp/api/character', { params: { key: ENV['USR_LOCAL_API_KEY'], message: response_json['result'], character_type: "dog"} }
+    roujin_res =RestClient.get 'https://chatbot-api.userlocal.jp/api/character', { params: { key: ENV['USR_LOCAL_API_KEY'], message: response_json['result'], character_type: "roujin"} }
     cat_res_json = JSON.parse(cat_res)
+    dog_res_json = JSON.parse(dog_res)
+    roujin_res_json = JSON.parse(roujin_res)
     p response_json['result']
     p cat_res_json["result"]
-    #p CGI.unescape(cat_res_json["result"])
+    p dog_res_json["result"]
+    p roujin_res_json["result"]
     response_json['status'] == "success" ? response_json['result'] : '通信エラー'
   end
 end
