6828b8f65956fe45edd5289bfcf0b0e5c705ceca
ShotaOnishi <showta.0804@gmail.com>
2016-11-08 02:25:28 +0900

    test

--- model/observers/message/local_message.rb
+++ model/observers/message/local_message.rb
@@ -5,10 +5,7 @@ class LocalMessage
     elsif Message.is_message?(event)
       if /(雑談)/ =~ event.message['text']
         event.message["text"] = event.message["text"].delete("雑談")
-        text = []
-        for i in 1..2 do
-          text[i] = get_user_local_bot_reply(event.message["text"])
-        end
+        text = get_user_local_bot_reply(event.message["text"])
         Message.reply(event, output(text))
       end
     end
@@ -19,29 +16,39 @@ class LocalMessage
       {
         type: "text",
         text: text[1].to_s
-      },
-      {
-        type: "text",
-        text: text[2].to_s
-      }
-    ]
-  end
+        },
+        {
+          type: "text",
+          text: text[2].to_s
+        }
+      ]
+    end
 
-  def get_user_local_bot_reply(word)
-    require 'rest-client'
-    response = RestClient.get 'https://chatbot-api.userlocal.jp/api/chat', { params: { key: ENV['USR_LOCAL_API_KEY'], message: CGI.escape(word)} }
-    response_json = JSON.parse(response)
-    cat_res =RestClient.get 'https://chatbot-api.userlocal.jp/api/character', { params: { key: ENV['USR_LOCAL_API_KEY'], message: response_json['result'] + "な", character_type: "cat"} }
-    dog_res =RestClient.get 'https://chatbot-api.userlocal.jp/api/character', { params: { key: ENV['USR_LOCAL_API_KEY'], message: response_json['result'] + "です", character_type: "dog"} }
-    roujin_res =RestClient.get 'https://chatbot-api.userlocal.jp/api/character', { params: { key: ENV['USR_LOCAL_API_KEY'], message: response_json['result'] + "だよ", character_type: "roujin"} }
-    cat_res_json = JSON.parse(cat_res)
-    dog_res_json = JSON.parse(dog_res)
-    roujin_res_json = JSON.parse(roujin_res)
-    p response_json['result']
-    p cat_res_json["result"]
-    p dog_res_json["result"]
-    p roujin_res_json["result"]
-    #response_json['status'] == "success" ? response_json['result'] : '通信エラー'
-    roujin_res_json["result"]
+    def get_user_local_bot_reply(word)
+      if /(犬)/ =~ word
+        word = word.delete("犬")
+        num = 1
+      elsif /(猫)/ =~ word
+        word = word.delete("猫")
+        num = 2
+      elsif /(老人)/ =~ word
+        word = word.delete("老人")
+        num = 3
+      else
+        num = 4
+      end
+      response = RestClient.get 'https://chatbot-api.userlocal.jp/api/chat', { params: { key: ENV['USR_LOCAL_API_KEY'], message: CGI.escape(word)} }
+      response_json = JSON.parse(response)
+      case num
+      when 1
+        res = RestClient.get 'https://chatbot-api.userlocal.jp/api/character', { params: { key: ENV['USR_LOCAL_API_KEY'], message: response_json['result'] + "な", character_type: "cat"} }
+      when 2
+        res = RestClient.get 'https://chatbot-api.userlocal.jp/api/character', { params: { key: ENV['USR_LOCAL_API_KEY'], message: response_json['result'] + "です", character_type: "dog"} }
+      when 3
+        res = RestClient.get 'https://chatbot-api.userlocal.jp/api/character', { params: { key: ENV['USR_LOCAL_API_KEY'], message: response_json['result'] + "だよ", character_type: "roujin"} }
+      when 4
+        res = response
+      end
+        res_json = JSON.parse(res)
   end
 end
