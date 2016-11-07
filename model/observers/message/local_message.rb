9a4e6a9e93806981673bfa2549bd788d0adc5b07
ShotaOnishi <showta.0804@gmail.com>
2016-11-08 01:22:35 +0900

    test

--- model/observers/message/local_message.rb
+++ model/observers/message/local_message.rb
@@ -15,14 +15,17 @@ class LocalMessage
       {
         type: "text",
         text: text
-      }
+      },
+      {
+        type: "text",
+        text: text
+      },
     ]
   end
 
   def get_user_local_bot_reply(word)
     require 'rest-client'
     response = RestClient.get 'https://chatbot-api.userlocal.jp/api/chat', { params: { key: ENV['USR_LOCAL_API_KEY'], message: CGI.escape(word)} }
-    p response
     response_json = JSON.parse(response)
     p response_json['result']
     response_json['status'] == "success" ? response_json['result'] : '通信エラー'
