1dfbad3d50ac2b6c29c4f0049b7b52bbef33c36a
ShotaOnishi <showta.0804@gmail.com>
2016-11-08 01:51:33 +0900

    test

--- model/observers/message/local_message.rb
+++ model/observers/message/local_message.rb
@@ -35,7 +35,7 @@ class LocalMessage
     require 'rest-client'
     response = RestClient.get 'https://chatbot-api.userlocal.jp/api/chat', { params: { key: ENV['USR_LOCAL_API_KEY'], message: CGI.escape(word)} }
     response_json = JSON.parse(response)
-    cat_res =RestClient.get 'https://chatbot-api.userlocal.jp/api/character', { params: { key: ENV['USR_LOCAL_API_KEY'], message: response['result'], character_type: "cat"} }
+    cat_res =RestClient.get 'https://chatbot-api.userlocal.jp/api/character', { params: { key: ENV['USR_LOCAL_API_KEY'], message: response_json['result'], character_type: "cat"} }
     cat_res_json = JSON.parse(cat_res)
     p response_json['result']
     p cat_res_json["result"]
