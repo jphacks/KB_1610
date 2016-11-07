1ceaa63575540fe97157c167ada697676921a090
ShotaOnishi <showta.0804@gmail.com>
2016-11-08 00:29:54 +0900

    test

--- model/observers/message/local_message.rb
+++ model/observers/message/local_message.rb
@@ -4,8 +4,7 @@ class LocalMessage
     if Message.is_postback?(event)
     elsif Message.is_message?(event)
       if /(雑談)/ =~ event.message['text']
-        event.message["text"].delete("雑談")
-        text = get_user_local_bot_reply(event.message["text"])
+        text = get_user_local_bot_reply(event.message["text"].delete("雑談"))
         Message.reply(event, output(text))
       end
     end
@@ -22,7 +21,7 @@ class LocalMessage
 
   def get_user_local_bot_reply(word)
     require 'rest-client'
-    response = RestClient.get 'https://chatbot-api.userlocal.jp/api/character', { params: { key: ENV['USR_LOCAL_API_KEY'], message: CGI.escape(word), character_type: "dog" } }
+    response = RestClient.get 'https://chatbot-api.userlocal.jp/api/chat', { params: { key: ENV['USR_LOCAL_API_KEY'], message: CGI.escape(word)} }
     response_json = JSON.parse(response)
     response_json['status'] == "success" ? response_json['result'] : '通信エラー'
   end
